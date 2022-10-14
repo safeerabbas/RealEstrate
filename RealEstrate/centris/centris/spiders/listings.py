dba28de794f1101034e79b79c20f37c9ae4bb58f
safeer <safeer.uol@gmail.com>
2022-10-14 08:15:42 -0800

    test

--- RealEstrate/centris/centris/spiders/listings.py
+++ RealEstrate/centris/centris/spiders/listings.py
@@ -2,7 +2,9 @@ import scrapy
 class ListingsSpider(scrapy.Spider):
     name = 'listings'
     allowed_domains = ['www.centris.ca']
-    start_urls = ['http://www.centris.ca/']
+
+    def start_requests(self):
+        pass
 
     def parse(self, response):
         pass
